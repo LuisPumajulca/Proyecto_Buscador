6485354239513a0734c9ca4ee32252de05672ce5
LuisPumajulca <pumajulca@hotmail.com>
2023-08-12 12:06:01 -0500

    Filtrar por Precio

--- js/app.js
+++ js/app.js
@@ -51,11 +51,11 @@ year.addEventListener('change', e => {
 })
 minimo.addEventListener('change', e => {
     datosBusqueda.minimo = e.target.value;
-
+    filtrarAuto();
 })
 maximo.addEventListener('change', e => {
     datosBusqueda.maximo = e.target.value;
-
+    filtrarAuto();
 })
 puertas.addEventListener('change', e => {
     datosBusqueda.puertas = e.target.value;
@@ -111,7 +111,7 @@ function llenarSelect(){
 
 // Función que filtra en base a la búsqueda
 function filtrarAuto() { // Función de alto nivel
-   const resultado = autos.filter( filtrarMarca ).filter( filtrarYear );
+   const resultado = autos.filter( filtrarMarca ).filter( filtrarYear ).filter( filtrarMinimo ).filter( filtrarMaximo );
 
     //console.log(resultado)
     mostrarAutos(resultado);
@@ -129,6 +129,22 @@ function filtrarYear(auto) {
     const { year } = datosBusqueda;
     if( year ) {
         return auto.year === year;
+    }
+    return auto;
+}
+
+function filtrarMinimo(auto) {
+    const { minimo } = datosBusqueda;
+    if( minimo ) {
+        return auto.precio >= minimo;
+    }
+    return auto;
+}
+
+function filtrarMaximo(auto) {
+    const { maximo } = datosBusqueda;
+    if( maximo ) {
+        return auto.precio <= maximo;
     }
     return auto;
 }
